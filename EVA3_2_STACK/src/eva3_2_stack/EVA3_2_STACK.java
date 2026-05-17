cdf9f65787b93673e875285b6e20cb1562fc5014
NocheII <L25550865@chihuahua2.tecnm.mx>
2026-05-17 00:33:13 -0600

    Add files via upload

---
+++ EVA3_2_STACK/src/eva3_2_stack/EVA3_2_STACK.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_2_stack;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA3_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A();
+        // TODO code application logic here
+    }
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        C();
+    }
+    
+    public static void C(){
+        int x = 100, y = 0, resu;
+        resu = x / y;
+        System.out.println("Resu x / y = " + resu);
+    }
+    
+}
